0f0a8f2edb5b91ca4667b01c7f41095ea40f767d
Ali <aliammarali125@gmail.com>
2021-11-17 14:23:37 +0300

    update docs

--- lib/src/battery_optimizations_observer.dart
+++ lib/src/battery_optimizations_observer.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 import 'package:flutter/widgets.dart';
 import 'package:optimization_battery/src/methodchannel.dart';
-
 class BatteryOptimizationsObserver extends StatelessWidget {
   final Widget Function(BuildContext, bool? isIgnored) builder;
+  /// widget help to listin to any change to BatteryOptimizations state 
+  /// only for android , on any other platofrm 'isIgnored' will be null
+  /// isIgnored will be null at start before complete load the state for the first time
+  /// then it will be re loaded every time user leave the app and return to it 
   const BatteryOptimizationsObserver({Key? key, required this.builder})
       : super(key: key);
 
@@ -17,9 +20,10 @@ class BatteryOptimizationsObserver extends StatelessWidget {
         : builder(context, null);
   }
 }
-
 class BatteryOptimizationsObserverAndroid extends StatefulWidget {
   final Widget Function(BuildContext, bool? isIgnored) builder;
+  
+  /// widget help to listin to any change to BatteryOptimizations state ( only for android)
   const BatteryOptimizationsObserverAndroid({Key? key, required this.builder})
       : super(key: key);
 
@@ -31,6 +35,7 @@ class BatteryOptimizationsObserverAndroid extends StatefulWidget {
 class _BatteryOptimizationsObserverAndroidState
     extends State<BatteryOptimizationsObserverAndroid>
     with WidgetsBindingObserver {
+  /// isIgnoringBatteryOptimizations
   bool? isIgnoringBatteryOptimizations;
 
   @override
@@ -49,7 +54,7 @@ class _BatteryOptimizationsObserverAndroidState
       reSetBatteryOptimizations();
     }
   }
-
+  /// re load the isIgnoringBatteryOptimizations value and update the widget
   void reSetBatteryOptimizations() {
     OptimizationBattery.isIgnoringBatteryOptimizations().then((value) {
       if (isIgnoringBatteryOptimizations != value) {
--- lib/src/methodchannel.dart
+++ lib/src/methodchannel.dart
@@ -4,13 +4,13 @@ import 'package:flutter/services.dart';
 
 class OptimizationBattery {
   static const MethodChannel _channel = MethodChannel('optimization_battery');
-
+  /// check if the app ignoring BatteryOptimizations
   static Future<bool> isIgnoringBatteryOptimizations() async {
     final reading =
         await _channel.invokeMethod('isIgnoringBatteryOptimizations');
     return reading!;
   }
-
+  /// open BatteryOptimizations setting page
   static Future<bool> openBatteryOptimizationSettings() async {
     final reading =
         await _channel.invokeMethod('openBatteryOptimizationSettings');
